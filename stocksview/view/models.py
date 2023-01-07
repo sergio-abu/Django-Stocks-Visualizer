30ab63736d2e35d8cfbbbcff17b2e5d3bf3b2470
Sergio A S <sergio-abu@proton.me>
2023-01-07 10:22:51 -0300

    first commit

---
+++ stocksview/view/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class StockData(models.Model):
+    symbol = models.TextField(null=True)
+    data = models.TextField(null=True)
+
+
+    def __str__(self):
+        return self.symbol
